379d436caa25bfdbc50f310f80abdd73457910f8
Charles-Nyabeze <charlesnnyabeze@gmail.com>
2022-02-05 01:34:53 -0500

    final page creation

---
+++ src/generatePage.js
@@ -0,0 +1,151 @@
+const generateManager = function (manager) {
+    return `
+<div class="column is-4">
+    <div class="card large">
+        <div class="card-content">
+            <div class="card-header">
+            <div class="content">
+                <p class="title is-4">${manager.name}</p>
+                <p class="subtitle is-6">Manager</p><i class="fas fa-home"></i>
+            </div>
+            </div>
+        
+            <div class="content">
+                <p class="id">ID: ${manager.id}</p>
+                <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+                <p class="office">Office Number: ${manager.officeNumber}</p>
+            </div>
+        </div>
+    </div>
+</div>
+    `;
+}
+
+// create Engineer card
+const generateEngineer = function (engineer) {
+    return `
+    <div class="column is-4">
+    <div class="card large">
+        <div class="card-content">
+            <div class="card-header">
+            <div class="content">
+                <p class="title is-4">${engineer.name}</p>
+                <p class="subtitle is-6">Engineer</p><i class="fas fa-home"></i>
+            </div>
+            </div>
+        
+            <div class="content">
+                <p class="id">ID: ${engineer.id}</p>
+                <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+            </div>
+        </div>
+    </div>
+</div>
+    `
+}
+
+// create Intern card 
+const generateIntern = function (intern) {
+    return `
+<div class="column is-4">
+    <div class="card large">
+        <div class="card-content">
+            <div class="card-header">
+            <div class="content">
+                <p class="title is-4">${intern.name}</p>
+                <p class="subtitle is-6">Intern</p><i class="fas fa-home"></i>
+            </div>
+            </div>
+        
+            <div class="content">
+                <p class="id">ID: ${intern.id}</p>
+                <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
+                <p class="school">School: ${intern.school}</p>
+            </div>
+        </div>
+    </div>
+</div>
+    `
+};
+
+// push array to page 
+generateHTML = (data) => {
+    // array for cards 
+    pageArray = []; 
+
+    for (let i = 0; i < data.length; i++) {
+        const employee = data[i];
+        const role = employee.getRole(); 
+
+
+        // call manager function
+        if (role === 'Manager') {
+            const managerCard = generateManager(employee);
+
+            pageArray.push(managerCard);
+        }
+
+        // call engineer function
+        if (role === 'Engineer') {
+            const engineerCard = generateEngineer(employee);
+
+            pageArray.push(engineerCard);
+        }
+
+        // call intern function 
+        if (role === 'Intern') {
+            const internCard = generateIntern(employee);
+
+            pageArray.push(internCard);
+        }
+        
+    }
+
+    // joining strings 
+    const employeeCards = pageArray.join('')
+
+    // return to generated page
+    const generateTeam = generateTeamPage(employeeCards); 
+    return generateTeam;
+
+}
+
+// generate html page 
+const generateTeamPage = function (employeeCards) {   
+  return`
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Team Profile</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+  <link rel="stylesheet" href="style.css">
+  <script src="https://kit.fontawesome.com/514093d1a0.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+        <header>
+        <nav class="navbar" id="navbar">
+            <span class="container" id="navbar-text">Team Profile</span>
+        </nav>
+        </header>
+      <main>
+          <div class="container">
+              <div class="row columns is-multiline" id="team-cards">
+                  <!--Team Cards-->
+                  ${employeeCards}
+              </div>
+          </div>
+      </main>
+      
+      </body>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+      </html>
+`;
+}
+
+// export to index
+module.exports = generateHTML; 
